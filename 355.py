5bb3c92784a4697deca4579e54b8b00a3d10e679
Chris Navrides <cninden@gmail.com>
2011-10-26 19:57:20 -0700

    getting closer

--- 355.py
+++ 355.py
@@ -1,5 +1,9 @@
-#!/usr/bin/python
-val = 10
+import time
+
+
+t0= time.clock()
+
+val =  30
 
 allPrimes = []
 allNumbers = []
@@ -16,17 +20,30 @@ for x in allPrimes:
         if y % x == 0 and y/x != 1:
             allPrimes.remove(y)
 
+#copy all primes 
+availablePrimes = list(allPrimes)
 #copy the list of all primes
-usedPrimes=list(allPrimes)
+usedPrimes = {}
+for p in allPrimes:
+    usedPrimes[p] = []
+
 myNumers=list(allNumbers)
 
+#Reverse all numbers
 allNumbers.reverse()
+
+#a list of combinations
+highestCombo={}
+
 
 prime1 = -1
 prime2 = -1
 for x in allNumbers:
     val = 0
+
     for y in allPrimes:
+        if y > x:
+            break
         if x%y == 0:
             val += 1
             if val == 1:
@@ -35,23 +52,62 @@ for x in allNumbers:
                 prime2 = y
         if val > 2:
             break
-    if val != 3:
-        if val == 1:
-            if prime1 in usedPrimes:
-                usedPrimes.remove(prime1)
-            else:
-                myNumers.remove(x)
-       # else:
-       #     if prime1 in usedPrimes and prime2 in usedPrimes:
-       #         usedPrimes.remove(prime1)
-       #         usedPrimes.remove(prime2)
-       #     else:
-       #         myNumers.remove(x)
+    if val > 0 and val < 3:
+        if val == 1 and prime1 in availablePrimes:
+            temp =  str(prime1)+ "-" + str(prime1)
+            availablePrimes.remove(prime1)
+        elif val == 2 and prime1 in availablePrimes and prime2 in availablePrimes:
+            temp = str(prime1)+ "-" + str(prime2)
+        else:
+            myNumers.remove(x)
+            continue
+        
+        if temp in highestCombo:
+            myNumers.remove(x)
+        else:
+            highestCombo[temp] = x
+            usedPrimes[prime1].append(x)
+            if val == 2:
+                usedPrimes[prime2].append(x)
     else:
         myNumers.remove(x)
 
-print "allPrimes: " + str(allPrimes)
-print "usedPrimes: " + str(usedPrimes)
-print "myNumbers: " + str(myNumers)
-answer = sum(myNumers) + 1
-print answer
+reverse = {}
+
+for x in highestCombo:
+    reverse[highestCombo[x]] = x
+
+
+finalNumbers = []
+finalPrimes = []
+for num in usedPrimes:
+    if len(usedPrimes[num]) == 1:
+        finalNumbers.append(usedPrimes[num][0])
+        finalPrimes.append(num)
+
+for x in finalPrimes:
+    del usedPrimes[x]
+    del reverse[highestCombo[str(x)+'-'+str(x)]]
+    del highestCombo[str(x)+'-'+str(x)]
+
+   
+#print "highestCombo: " + str(highestCombo)
+#print "reverse: " + str(reverse)
+#print "allPrimes: " + str(allPrimes)
+#print "usedPrimes: " + str(usedPrimes)
+#print "myNumbers: " + str(myNumers)
+
+
+answer = sum(finalNumbers) + 1
+print "answer: " + str(answer)
+print "finalNumvers: " + str(finalNumbers)
+
+
+
+print
+print "len(highestCombo): " + str(len(highestCombo))
+#print "len(allPrimes): " + str(len(allPrimes))
+#print "len(myNumers): " + str(len(myNumers))
+print
+
+print "time taken: " + str(time.clock() - t0) + " seconds"
