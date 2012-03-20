dc61d7007410176140e0df2dbc4eeaf7f4461da0
Chris Navrides <cninden@gmail.com>
2012-03-20 01:13:48 -0700

    adding comments

--- python/20.py
+++ python/20.py
@@ -1,12 +1,22 @@
 #/usr/bin/bash
 
+#value of the result of doing a factorial 
 num = 1
-for i in range(100):
+
+#size of factorial
+size = 100
+
+#find factorial
+for i in range(size):
 	num *= (i+1)
 
+#convert to string
 numString = str(num)
 
+#value of the result of adding digits
 finalValue = 0
+
+#go through each digit
 for number in numString:
 	finalValue += int(number)
 
