dcc1a7d3b65a2f3046ab4d94becf698973a878d1
Chris Navrides <cninden@gmail.com>
2012-03-20 17:24:56 -0700

    cleaning up 15

--- python/15.py
+++ python/15.py
@@ -2,24 +2,16 @@
 
 total = 0
 size = 20
-# This should be using DP instead of recursion
-def go():
-	count (0,0)
 
+# go through all possibilitiesand when it hitsthe end increase by 1
 def count(x, y):
 	global total
-#	print "(" + str(x) + "," + str(y) + ")" + " = " + str(total)
 	if x < size:
 		count(x+1, y)
 	if y < size: 
 		count (x, y+1) 
 	if x == y == size:
 		total += 1
-'''
-matrix = [ [ 0 for i in range(size) ] for j in range(size) ]
-for i in range(size):
-	matrix[i] = []
-'''
+count(0,0)
 
-go()
 print total
