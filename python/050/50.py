13a2e859f33ed398f1083a8352c8c412523c0e60
Chris Navrides <cninden@gmail.com>
2012-04-10 19:34:31 -0700

    adding problem 50, takes 20 minutes but it found it

---
+++ python/050/50.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from common import getPrimes
+
+#get all primes below 1,000,000
+primes = getPrimes(1000000)
+
+
+maxCount = 0
+maxPrime = -1
+
+#prime to look at
+for p in primes:
+	#index variable	
+	i = 0
+	
+	#count of how many primes used
+	count = 0	
+
+	#sum of the primes so far
+	total = 0
+
+	#Start index value
+	start = 0
+	tooHigh = p/2
+
+	#keep in loop until the prime is half of the one being looked at
+	while primes[i] <=  tooHigh:
+		#add currentPrime to total & increment count
+		total += primes[i]
+		count += 1
+		
+		#if found break		
+		if total == p:
+			break
+		#if it's too high, take off the begining ones 
+		if total > p: 
+			#loop until either we have taken too many off or the total is 
+			#below the prime
+			while count > 0 and total > p:
+				total -= primes[start]
+				start += 1
+				count -= 1
+			#if it is now equal break
+			if total == p:
+				break
+
+		#increment index
+		i += 1
+	#if a combination wasfound and the number of primes was greater than current max	
+	if total == p and count > maxCount:
+		maxCount = count
+		maxPrime = p
+
+print maxPrime	
+			
