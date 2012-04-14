73b328cec292768c3aa78fa138677f29b9c015a5
Chris Navrides <cninden@gmail.com>
2012-04-14 11:34:28 -0700

    fixing error in common

--- python/common.py
+++ python/common.py
@@ -12,30 +12,21 @@ def memo(func):
 
 #gets the array of prime numbers  below the value
 def getPrimes(maxVal):
-	primes = [2,3]
+	primes = [2,3,5]
 	nums = []
 	#Initialize list of values
-	for i in range((maxVal+1)):
-		if i % 2 == 0 or i % 3 == 0 or i  < 3:
-			nums.append(-1)
-		else:
-			nums.append(0)
-	count = 0	
+	for i in range(3, (maxVal+1)):
+		if i % 2 != 0 and i % 3 != 0 and i % 5 != 0:
+			nums.append(i)
 	#find the primes using the sieve of erodes
 	for j in nums:
-		if j != -1:
-			isPrime = True
-			for p in primes:
-				if count % p == 0:
-					isPrime = False
-					break
-			if isPrime == True:
-				primes.append(count)
-				tmp = count
-				while tmp < maxVal:
-					nums[tmp] = -1
-					tmp += count
-		count += 1
+		isPrime = True
+		for p in primes:
+			if j % p == 0:
+				isPrime = False
+				break
+		if isPrime == True:
+			primes.append(j)
 	return primes
 
 #get the factors of a number
